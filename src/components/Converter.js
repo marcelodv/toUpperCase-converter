9014b7484f05e1ff9b7872aeba9670b9576da7eb
Marcelo Santos <marcelo.santos.dev@gmail.com>
2020-11-23 10:38:16 -0300

    Finish v1

--- src/components/Converter.js
+++ src/components/Converter.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import '../styles/Converter.css'
 
 function Conversor() {
     const [text, setText] = useState('')
@@ -6,15 +7,24 @@ function Conversor() {
     return (
         <div>
             <h1>To Upper Case</h1>
-            <input type="text" id="textInput" placeholder="Insert the text"/>
-            <button onClick={() => {
-                var t1 = document.getElementById('textInput').value.toUpperCase()
-                setText(t1)
-            }}>
-                To Up
-            </button>
-            <br/><br></br>
-            <label>{text}</label>
+            <div className="sectionOne">
+                <input type="text" id="textInput" placeholder="Insert the text here"/>
+                <button onClick={() => {
+                    var t1 = document.getElementById('textInput').value.toUpperCase()
+                    if (t1 == '') {
+                        alert("Por favor, preencha o campo de texto.")
+                    }
+                    else {
+                        setText(t1)
+                    }
+                    
+                }}>
+                    To Up
+                </button>
+            </div>
+            <div className="result">
+            <label>{text == '' ? 'RESULT OF OF CONVERSION' : text}</label>
+            </div>
         </div>
     );
 }
