ce54f934e237a9a26ede1df66d8e6db517e850cb
Marcelo Santos <marcelo.santos.dev@gmail.com>
2020-11-23 11:11:31 -0300

    Code Organized

--- src/components/Converter.js
+++ src/components/Converter.js
@@ -9,21 +9,19 @@ function Conversor() {
             <h1>To Upper Case</h1>
             <div className="sectionOne">
                 <input type="text" id="textInput" placeholder="Insert the text here"/>
-                <button onClick={() => {
-                    var t1 = document.getElementById('textInput').value.toUpperCase()
-                    if (t1 == '') {
+                <button type="button" onClick={() => {
+                    const textInserted = document.getElementById('textInput').value
+                    const textConverted = textInserted.toUpperCase()
+                    if (textConverted === '') {
                         alert("Por favor, preencha o campo de texto.")
-                    }
-                    else {
-                        setText(t1)
+                    } else {
+                        setText(textConverted)
                     }
                     
-                }}>
-                    To Up
-                </button>
+                }} >TO UP</button>
             </div>
             <div className="result">
-            <label>{text == '' ? 'RESULT OF OF CONVERSION' : text}</label>
+                <label>{text === '' ? 'RESULT OF CONVERSION' : text}</label>
             </div>
         </div>
     );
